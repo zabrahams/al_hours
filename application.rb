06eed43f002eddbef3243984e395ef9909d160d5
Zach Abrahams <zachabrahams@gmail.com>
2016-02-02 01:25:51 -0700

    completes put form

--- application.rb
+++ application.rb
@@ -17,6 +17,7 @@ put "/hours" do
   redis = Redis.new(:host => "127.0.0.1", :port => 6379)
   hours = redis.get("hours")
   hours ||= 0
+  puts params
   new_hours = hours.to_i +  params["hours"].to_i
   redis.set("hours", new_hours)
   { hours: new_hours }.to_json
--- public/app.js
+++ public/app.js
@@ -3,29 +3,69 @@
         window.Hours = {};
     }
 
-    Hours.horus = null;
+    Hours.hours = 0;
+    Hours.WAGE = 15;
 
     Hours.refresh = function () {
       $("#currentHours").text(Hours.hours)
     }
 
+    Hours.refreshPay = function () {
+      $("#currentPay").text("$" + (Hours.hours * Hours.WAGE))
+    }
+
+    Hours.deleteHours = function () {
+      $.ajax({
+        dataType: "json",
+        method: "DELETE",
+        url: "/hours",
+        success: function (resp) {
+          Hours.hours = resp.hours;
+          Hours.refresh();
+          Hours.refreshPay();
+        }
+      });
+    }
+
+    Hours.getHours = function () {
+      $.ajax({
+        dataType: "json",
+        method: "GET",
+        url: "/hours",
+        success: function (resp) {
+          Hours.hours = resp.hours;
+          Hours.refresh();
+          Hours.refreshPay();
+        }
+      });
+    }
+
+    Hours.putHours = function (hoursToAdd) {
+      $.ajax({
+        dataType: 'json',
+        method: "PUT",
+        url: "/hours",
+        data: {hours: hoursToAdd},
+        success: function (resp) {
+          Hours.hours = resp.hours;
+          Hours.refresh();
+          Hours.refreshPay();
+        }
+      })
+    }
+
     $(document).ready(function () {
+      Hours.getHours();
 
       $("#resetButton").on("click", function (e) {
         e.preventDefault();
+        Hours.deleteHours();
+      });
 
-        $.ajax({
-          dataType: "json",
-          method: "DELETE",
-          url: "/hours",
-          success: function (resp) {
-            console.log(resp)
-            console.log(resp.hours)
-            Hours.hours = resp.hours
-            Hours.refresh()
-          }
-        })
+      $("#addHours").on("submit", function (e) {
+        e.preventDefault();
+        var hoursToAdd = $(e.target[0]).val();
+        Hours.putHours(hoursToAdd)
       })
-
     });
 })();
